d3d7f9c22dec736be2df13b9cf59e2d212386df0
Frank de Pijper <fdepijper@wim4you.nl>
2016-10-11 05:13:40 +0200

    Added settings

--- PinEvents.SeatsIo/PinEvents.SeatsIo.Data/Settings/SysSettingBm.cs
+++ PinEvents.SeatsIo/PinEvents.SeatsIo.Data/Settings/SysSettingBm.cs
@@ -12,7 +12,12 @@ namespace PinEvents.SeatsIo.Settings
         public IDictionary<string,string> GetValues()
         {
             IDictionary<string, string> values = new Dictionary<string, string>();
-
+            values.Add("seats.io.baseuri", "https://app.seats.io");
+            values.Add("secretKey", "your-secret-key");
+            values.Add("publicKey", "your-public-key");
+            values.Add("designerKey", "your-designer-key");
+            values.Add("chartKey", "your-chart-key");
+            values.Add("eventKey", "your-event-key");
             return values;
         }
     }
