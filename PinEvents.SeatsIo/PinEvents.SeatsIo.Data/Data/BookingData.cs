ec56ec196726886bdf0746d184e39927ac79fdc0
Frank de Pijper <fdepijper@wim4you.nl>
2016-10-11 05:33:53 +0200

    Modified BookingData

--- PinEvents.SeatsIo/PinEvents.SeatsIo.Data/Data/BookingData.cs
+++ PinEvents.SeatsIo/PinEvents.SeatsIo.Data/Data/BookingData.cs
@@ -1,14 +1,36 @@
-﻿namespace PinEvents.SeatsIo.Data
-{
-    using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace PinEvents.SeatsIo.Data
+{
     public class BookingData
     {
-        public string eventKey { get; set; }
+        public long? EventId { get; set; }
+        public string EventKey { get; set; }
         public string secretKey { get; set; }
-        public string orderId { get; set; }
         public string reservationToken { get; set; }
-        public List<string> objects { get; set; }
+        public string orderId { get; set; }
+
+        public List<BookObject> free { get; set; }
+        public List<BookObject> booked { get; set; }
+        public List<BookObject> reservedByToken { get; set; }
+
+        public class BookObject
+        {
+            public long? TicketTypeId { get; set; }
+            public string uuid { get; set; }
+            public string label { get; set; }
+            public string status { get; set; }
+            public string objectType { get; set; }
+            public string category { get; set; }
+            public int? categoryKey { get; set; }
+            public string categoryLabel { get; set; }
+            public int? capacity { get; set; }
+            public int? numBooked { get; set; }
+        }
     }
 
     public class EventDetails
